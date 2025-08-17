820f138cdbc6999b7cb99b0d2197f188a2e90a38
forgeronduweb <forgeronduweb@gmail.com>
2025-08-17 15:16:58 +0000

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio',
     JWT_SECRET: process.env.JWT_SECRET || 'votre_secret_jwt',
     NODE_ENV: process.env.NODE_ENV || 'development',
-    CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:5173',
+    CORS_ORIGIN: process.env.CORS_ORIGIN || 'https://personal-portfolio-353x.onrender.com',
     ADMIN_CORS_ORIGIN: process.env.ADMIN_CORS_ORIGIN || 'http://localhost:5174'
 };
+
