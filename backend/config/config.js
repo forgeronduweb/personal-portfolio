cae3268122cde25e275b0aea1b6c91de349678eb
forgeronduweb <forgeronduweb@gmail.com>
2025-08-23 18:30:42 +0000

    Add files via upload

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/portfolio',
+    MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET || 'votre_secret_jwt',
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:5173',
     ADMIN_CORS_ORIGIN: process.env.ADMIN_CORS_ORIGIN || 'http://localhost:5174'
 };
